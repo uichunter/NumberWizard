9e78677c094e69ba0dfd044ec60cb49ce962cd69
uichunter <pankettingpanda@gmail.com>
2016-03-03 18:08:04 -0600

    Add initial.

---
+++ Assets/Scripts/LvManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LvManager : MonoBehaviour {
+	public void LvLoader (string name){
+		Debug.Log("==== "+"Loaded for "+name+" ====");
+		SceneManager.LoadScene(name);
+	}
+
+	public void LvLoader_quit (string name){
+		Debug.Log("==== "+"Quited for "+name+" ====");
+		SceneManager.LoadScene(name);
+	}
+
+	public void QuitRequest (){
+		Application.Quit();
+	}
+
+	}
+
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+	public int max;
+	public int min;
+	public int guess;
+	public int maxGuessAllowed=10;
+	public int guessCoin;
+	public Text text;
+	public Text guessNumberText;
+
+	// Use this for initialization
+	void Start () {
+		/*
+		Problem: I can not passing the value if i use
+		max=1000;
+		min=1;
+		guess=setGuess(max,min,guess);
+		in showInfo().
+		I have to call them in Start().
+		*/
+		startGame();
+	}
+
+	public void startGame(){
+		max=1000;
+		min=1;
+		setGuessCoin(this.maxGuessAllowed);
+		//guessCoin=maxGuessAllowed;
+
+		calculate(this);
+	}
+
+	public int setGuessCoin(int setGuessCoin){
+		this.guessCoin=setGuessCoin;
+		return setGuessCoin;
+	}
+
+	int setGuess (NumberWizard number)
+	{
+		guessCoin = guessCoin - 1;
+		guessNumberText.text=guessCoin.ToString();
+		int period = number.max - number.min;
+		int result;
+	
+		int ra = Random.Range (-(period/50), (period/50));
+		Debug.Log(ra);
+		result = (number.max+number.min)/2+ra;
+
+		return result;
+	}
+
+	public void higher (){
+		max=guess;
+		calculate(this);
+	}
+
+	public void lower (){
+		min=guess;
+		calculate(this);
+	} 
+
+	void calculate (NumberWizard number)
+	{
+		if (number.guessCoin > 0) {
+			number.guess = setGuess (number);
+			number.text.text=number.guess.ToString();
+			//Debug.Log(this.guessCoin);
+		} else {
+			
+			SceneManager.LoadScene("Win");
+		}
+	}
+}
